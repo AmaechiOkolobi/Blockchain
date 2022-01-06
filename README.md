9dbaaefd880e6e9ef5b89a54f4c4cb3147be2f37
AmaechiOkolobi <61935026+AmaechiOkolobi@users.noreply.github.com>
2022-01-06 16:19:46 +0000

    crowdfunding dapp

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-"# Blockchain" 
-"Crowdfunding DApp" 
+ "Crowdfunding DApp" 
---
+++ ethereum/web3.js
@@ -0,0 +1,17 @@
+import Web3 from "web3";
+ 
+let web3;
+ 
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // We are in the browser and metamask is running.
+  window.ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3(window.ethereum);
+} else {
+  // We are on the server *OR* the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/v3/f99eb66cb18347f4b6e16a1657ffdaed'
+  );
+  web3 = new Web3(provider);
+}
+ 
+export default web3;
