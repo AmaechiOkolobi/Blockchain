b83814459cbc6c98b11ce0e1fb379aecfb86bb49
AmaechiOkolobi <61935026+AmaechiOkolobi@users.noreply.github.com>
2022-05-14 12:24:27 +0100

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xa8Fb3Eae4142Feb4aDF461e63a6E02a130fa7277'
+  ''
 );
 
 export default instance;
